a6e298411ec3a998908f99553272230709b0cc32
RomainBadamoBarthelemy <https://github.com/AlanDamotte/Boole.git>
2015-02-27 09:28:14 +0100

    tentative de début de do_UserThreadCreate

--- code/userprog/userthread.cc
+++ code/userprog/userthread.cc
@@ -6,6 +6,16 @@
 #include "system.h"
 
 int do_UserThreadCreate(int f, int arg){
+	Thread t = new Thread("UserThread");
+
+	if(t==NULL){
+		return -1;
+	}
+
+	t->Fork((VoidFunctionPtr)f,arg);
+
+	//TODO
+
 	return 0;
 }
 
